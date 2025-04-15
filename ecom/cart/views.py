b013fbf6e327d8827c2e5bc3e0a2c3b925d35879
KrishYASWANT <yaswantkrish321@gmail.com>
2025-04-15 12:21:17 +0530

    Added Total and Remove From Cart

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -9,7 +9,8 @@ def cart_summary(request):
     cart =Cart(request)
     cart_products = cart.get_prods
     quantities = cart.get_quants
-    return render(request, 'cart_summary.html', {"cart_products":cart_products , "quantities":quantities}) 
+    totals = cart.cart_total()
+    return render(request, 'cart_summary.html', {"cart_products":cart_products , "quantities":quantities, "totals":totals}) 
 
 
 
@@ -37,8 +38,27 @@ def cart_add(request):
     
     
 def cart_delete(request): 
-    pass 
-
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id')) 
+        # Call delete function on Cart
+        cart.delete(product=product_id)
+        response = JsonResponse({'product':product_id})
+        #return redirect('cart_summary')
+        messages.success(request, ("The item has been Removed From your Cart..."))
+        return response
 
 def cart_update (request): 
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty':product_qty})
+        #return redirect('cart_summary')
+        messages.success(request, ("Your Cart Has Been Updated..."))
+        return response
